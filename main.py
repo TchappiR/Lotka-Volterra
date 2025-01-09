6b32731f7680b95235244b717180cb4db0f9b77a
TchappiR <tchappiromaric@gmail.com>
2025-01-09 23:55:21 +0100

    che=anging parameters

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ time = [0]
 lapin = [1]
 renard = [2]
 
-alpha, beta, delta, gama = 1/3, 1/3, 1/3, 1/3
+alpha, beta, delta, gama = 2/3, 4/3, 1, 1
 step = 0.001
 
-for _ in range(10):
+for _ in range(100_000):
     nex_value_time = time[-1] + step
     new_value_lapin = (lapin[-1] * (alpha - beta * renard[-1])) * step  + lapin[-1]
     new_value_renard = (renard[-1] * (delta * lapin[-1] - gama)) * step  + renard[-1]
@@ -17,6 +17,9 @@ for _ in range(10):
     renard.append(new_value_renard)
     
 plt.figure(figsize=(15, 6))
-plt.plot(time, lapin, "b-")
-plt.plot(time, renard, "r-")
+plt.plot(time, lapin, "b-", label="Lapins")
+plt.plot(time, renard, "r-", label="Renards")
+plt.xlabel("Temps (en Mois)")
+plt.ylabel("Population")
+plt.legend()
 plt.show()
