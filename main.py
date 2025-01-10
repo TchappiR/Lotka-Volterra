a9e1b1274146155df16b0ad44a9ad0579b78be43
TchappiR <tchappiromaric@gmail.com>
2025-01-10 02:43:07 +0100

    calculate mean square error

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from sklearn.metrics import *
 
 time = [0]
 lapin = [1]
@@ -7,6 +9,9 @@ renard = [2]
 
 alpha, beta, delta, gama = 2/3, 4/3, 1, 1
 step = 0.001
+
+fichier = "populations_lapins_renards.csv"
+df = pd.read_csv(fichier)
 
 for _ in range(100_000):
     nex_value_time = time[-1] + step
@@ -23,7 +28,20 @@ lapin *= 1_000
 renard = np.array(renard)
 renard *= 1_000
 
-    
+
+#Calcul de la MSE 
+
+real_lapin_value = np.array(df["lapin"])
+real_renard_value = np.array(df["renard"])
+
+mse_lapin = mean_squared_error(real_lapin_value, lapin[:1_000])
+mse_renard = mean_squared_error(real_renard_value, renard[:1_000])
+
+print(f"MSE lapin = {mse_lapin}")
+print(f"MSE renard = {mse_renard}")
+
+
+# Graphique
 plt.figure(figsize=(15, 6))
 plt.plot(time, lapin, "b-", label="Lapins")
 plt.plot(time, renard, "r-", label="Renards")
